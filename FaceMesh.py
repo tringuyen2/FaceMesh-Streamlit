4a99af55ebb5a2452d5d169526d34e5d9b5f7074
Tri Nguyen <nguyentrungtri989@gmail.com>
2022-01-06 09:04:07 +0700

    first commit

---
+++ FaceMesh.py
@@ -0,0 +1,75 @@
+#Created by MediaPipe
+#Modified by Augmented Startups 2021
+#Face Mesh Landmarks in 5 Minutes
+#Watch 5 Minute Tutorial at www.augmentedstartups.info/YouTube
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_face_mesh = mp.solutions.face_mesh
+import time
+
+# For static images:
+# drawing_spec=mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+# with mp_face_mesh.FaceMesh(
+#     static_image_mode=True,
+#     max_num_faces=1,
+#     min_detection_confidence=0.5) as face_mesh:
+
+#     image = cv2.imread('face.jpg')  #Insert your Image Here
+#     # Convert the BGR image to RGB before processing.
+#     results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+#     if not results.multi_face_landmarks:
+#         print("Continue")
+#     annotated_image = image.copy()
+#     for face_landmarks in results.multi_face_landmarks:
+#       print('face_landmarks:', face_landmarks)
+#       mp_drawing.draw_landmarks(
+#           image=annotated_image,
+#           landmark_list=face_landmarks,
+#           connections=mp_face_mesh.FACEMESH_CONTOURS,
+#           landmark_drawing_spec=drawing_spec,
+#           connection_drawing_spec=drawing_spec)
+#     cv2.imwrite(r'mesh.png', annotated_image)
+
+## For webcam input:
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+cap = cv2.VideoCapture(0)
+prevTime = 0
+with mp_face_mesh.FaceMesh(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as face_mesh:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = face_mesh.process(image)
+
+    # Draw the face mesh annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_face_landmarks:
+      for face_landmarks in results.multi_face_landmarks:
+        mp_drawing.draw_landmarks(
+            image=image,
+            landmark_list=face_landmarks,
+            connections=mp_face_mesh.FACEMESH_CONTOURS,
+            landmark_drawing_spec=drawing_spec,
+            connection_drawing_spec=drawing_spec)
+    currTime = time.time()
+    fps = 1 / (currTime - prevTime)
+    prevTime = currTime
+    cv2.putText(image, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 196, 255), 2)
+    cv2.imshow('MediaPipe FaceMesh', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release
+#Watch Tutorial at www.augmentedstartups.info/YouTube
